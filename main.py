9a78b606300c2111a3ba492fc2e6268775ae7877
Vinnichenko-Ivan <fox4080@gmail.com>
2021-03-29 19:52:45 +0700

    Use black(авто форматирование кода)

--- main.py
+++ main.py
@@ -1,62 +1,72 @@
 import pygame
 import random
 from random import randint
+
 WIDTH = 800
 HEIGHT = 800
 FPS = 10
 
 CELLCOUNTX = 40
 CELLCOUNTY = 40
-CELLSIZEXPX = WIDTH//CELLCOUNTX
-CELLSIZEXPY = HEIGHT//CELLCOUNTY
+CELLSIZEXPX = WIDTH // CELLCOUNTX
+CELLSIZEXPY = HEIGHT // CELLCOUNTY
 
-FOODCOLOR = (100,0 ,0)
+FOODCOLOR = (100, 0, 0)
 LINECOLOR = (100, 100, 100)
 SNAKECOLOR = (0, 100, 0)
 BLACK = (0, 0, 0)
 
-#--------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------
 class Food:
-    x = randint(0,CELLCOUNTX - 1)
-    y = randint(0,CELLCOUNTY - 1)
+    x = randint(0, CELLCOUNTX - 1)
+    y = randint(0, CELLCOUNTY - 1)
+
     def reGen(self):
         self.x = randint(0, CELLCOUNTX - 1)
         self.y = randint(0, CELLCOUNTY - 1)
-#--------------------------------------------------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------------------------------------------------
 class Snake:
-    segments=[[randint(0,CELLCOUNTX - 1),randint(0,CELLCOUNTY - 1)]]
+    segments = [[randint(0, CELLCOUNTX - 1), randint(0, CELLCOUNTY - 1)]]
     size = 1
-    direction = 0 #0 - ^ 1 - -> 2 - \/ 3 - <-
-    def changeDirection(self,buffDirection):
+    direction = 0  # 0 - ^ 1 - -> 2 - \/ 3 - <-
+
+    def changeDirection(self, buffDirection):
         self.direction = buffDirection
 
-    def step(self, newDirection = direction):
-        if(abs(self.direction - newDirection) != 2 and newDirection != -1):
+    def step(self, newDirection=direction):
+        if abs(self.direction - newDirection) != 2 and newDirection != -1:
             self.direction = newDirection
-        for i in range(0,len(self.segments) - 1):
+        for i in range(0, len(self.segments) - 1):
             self.segments[i][0] = self.segments[i + 1][0]
             self.segments[i][1] = self.segments[i + 1][1]
-        if (self.direction == 0):
+        if self.direction == 0:
             self.segments[len(snake.segments) - 1][1] -= 1
-        if (self.direction == 1):
+        if self.direction == 1:
             self.segments[len(snake.segments) - 1][0] += 1
-        if (self.direction == 2):
+        if self.direction == 2:
             self.segments[len(snake.segments) - 1][1] += 1
-        if (self.direction == 3):
+        if self.direction == 3:
             self.segments[len(snake.segments) - 1][0] -= 1
 
-    def eat(self, newDirection = direction):
-        if(abs(self.direction - newDirection) != 2 and newDirection != -1):
+    def eat(self, newDirection=direction):
+        if abs(self.direction - newDirection) != 2 and newDirection != -1:
             self.direction = newDirection
         self.size += 1
-        self.segments.append([self.segments[len(snake.segments) - 1][0],self.segments[len(snake.segments) - 1][1]])
-        if (self.direction == 0):
+        self.segments.append(
+            [
+                self.segments[len(snake.segments) - 1][0],
+                self.segments[len(snake.segments) - 1][1],
+            ]
+        )
+        if self.direction == 0:
             self.segments[len(snake.segments) - 1][1] -= 1
-        if (self.direction == 1):
+        if self.direction == 1:
             self.segments[len(snake.segments) - 1][0] += 1
-        if (self.direction == 2):
+        if self.direction == 2:
             self.segments[len(snake.segments) - 1][1] += 1
-        if (self.direction == 3):
+        if self.direction == 3:
             self.segments[len(snake.segments) - 1][0] -= 1
 
     def reGen(self):
@@ -67,36 +77,76 @@ class Snake:
 
     def testCollision(this):
         for i in range(0, len(snake.segments) - 1):
-            if (snake.segments[i][0] == snake.segments[len(snake.segments) - 1][0] and snake.segments[i][1] ==
-                    snake.segments[len(snake.segments) - 1][1]):
+            if (
+                snake.segments[i][0] == snake.segments[len(snake.segments) - 1][0]
+                and snake.segments[i][1] == snake.segments[len(snake.segments) - 1][1]
+            ):
                 return True
         return False
-#--------------------------------------------------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------------------------------------------------
 def drawFildGrid(screen):
     for x in range(CELLSIZEXPX, WIDTH, CELLSIZEXPX):
-        pygame.draw.line(screen, LINECOLOR, (x,0), (x, WIDTH))
+        pygame.draw.line(screen, LINECOLOR, (x, 0), (x, WIDTH))
     for y in range(CELLSIZEXPY, HEIGHT, CELLSIZEXPY):
         pygame.draw.line(screen, LINECOLOR, (0, y), (HEIGHT, y))
 
-def drawSnake(screen,snake):
-    for i in range(0,len(snake.segments) - 1):
-        pygame.draw.rect(screen, SNAKECOLOR, (snake.segments[i][0] * CELLSIZEXPX + 1, snake.segments[i][1] * CELLSIZEXPY + 1, CELLSIZEXPX - 1, CELLSIZEXPY - 1))
-    pygame.draw.rect(screen, SNAKECOLOR, (snake.segments[len(snake.segments) - 1][0] * CELLSIZEXPX + 1, snake.segments[len(snake.segments) - 1][1] * CELLSIZEXPY + 1, CELLSIZEXPX - 1, CELLSIZEXPY - 1))
 
-def drawFood(screen,food):
-    pygame.draw.rect(screen, FOODCOLOR, (food.x * CELLSIZEXPX + 1, food.y * CELLSIZEXPY + 1, CELLSIZEXPX - 1, CELLSIZEXPY - 1))
+def drawSnake(screen, snake):
+    for i in range(0, len(snake.segments) - 1):
+        pygame.draw.rect(
+            screen,
+            SNAKECOLOR,
+            (
+                snake.segments[i][0] * CELLSIZEXPX + 1,
+                snake.segments[i][1] * CELLSIZEXPY + 1,
+                CELLSIZEXPX - 1,
+                CELLSIZEXPY - 1,
+            ),
+        )
+    pygame.draw.rect(
+        screen,
+        SNAKECOLOR,
+        (
+            snake.segments[len(snake.segments) - 1][0] * CELLSIZEXPX + 1,
+            snake.segments[len(snake.segments) - 1][1] * CELLSIZEXPY + 1,
+            CELLSIZEXPX - 1,
+            CELLSIZEXPY - 1,
+        ),
+    )
 
-def testEating(food,snake):
-    if(food.x == snake.segments[len(snake.segments) - 1][0] and food.y == snake.segments[len(snake.segments) - 1][1]):
+
+def drawFood(screen, food):
+    pygame.draw.rect(
+        screen,
+        FOODCOLOR,
+        (
+            food.x * CELLSIZEXPX + 1,
+            food.y * CELLSIZEXPY + 1,
+            CELLSIZEXPX - 1,
+            CELLSIZEXPY - 1,
+        ),
+    )
+
+
+def testEating(food, snake):
+    if (
+        food.x == snake.segments[len(snake.segments) - 1][0]
+        and food.y == snake.segments[len(snake.segments) - 1][1]
+    ):
         return True
     return False
 
+
 def testOut(snake):
     for n in snake.segments:
-        if(n[0] > CELLCOUNTX - 1 or n[0] < 0 or n[1] > CELLCOUNTY - 1 or n[1] < 0):
+        if n[0] > CELLCOUNTX - 1 or n[0] < 0 or n[1] > CELLCOUNTY - 1 or n[1] < 0:
             return True
     return False
-#--------------------------------------------------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------------------------------------------------
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -105,7 +155,7 @@ clock = pygame.time.Clock()
 running = True
 snake = Snake()
 food = Food()
-#--------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------
 while running:
     clock.tick(FPS)
     newDirection = -1
@@ -126,10 +176,10 @@ while running:
             elif event.key == pygame.K_j:
                 FPS -= 5
 
-    if(testOut(snake) or snake.testCollision()):
+    if testOut(snake) or snake.testCollision():
         snake.reGen()
 
-    if(testEating(food,snake)):
+    if testEating(food, snake):
         snake.eat(newDirection)
         food.reGen()
     else:
